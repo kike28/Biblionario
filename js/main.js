5b59023df28121e5030e536c5f87e84490fcbf20
unknown <Administrador@MP4108080770766.procuraduria.local>
2017-11-16 11:33:53 -0500

    Mejoras en la escogencia de respuestas. Agregar preguntas en quiestion.js en formato json.

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@ var app = new Vue({
       respuestas:[{'': ''},{'': ''},{'': ''},{'': ''}],
       pregunta:[],
       indice: 10,
-      respCorrecta:false
+      respCorrecta:false,
+      desactivo:true
 
     },
     methods: {
@@ -14,8 +15,8 @@ var app = new Vue({
       },
 
       cargarPregunta: function(num){
-        this.pregunta=nivel1[num]
-        this.respuestas=nivel1[num].respuestas
+        this.pregunta=nivel1[num];
+        this.respuestas=nivel1[num].respuestas;
       },
 
       seleccionRespuesta: function(resp){
@@ -46,48 +47,60 @@ var app = new Vue({
           }
 
         
-        
-
-
       },
 
-      marcarRespuesta: function(num){
-        if(this.indice < 4){
-          document.getElementById('btnRevelarRespuesta').classList.remove('disabled');
-          
+      marcarRespuesta: function(num, event){
+        respMarcada = 'respuesta'+(num+1);
+        numIndice=num+1;
+        respuesta1 = document.getElementById('respuesta1');
+        respuesta2 = document.getElementById('respuesta2');
+        respuesta3 = document.getElementById('respuesta3');
+        respuesta4 = document.getElementById('respuesta4');
+        btnRevelar = document.getElementById('btnRevelarRespuesta');
+
+        // alert(Math.floor( Math.random() * (10 - 1)) + 1); Sacar Numeros enteros aleatorios
+
+  
+        if(document.getElementById(respMarcada).classList.contains('activo')){
+          document.getElementById(respMarcada).classList.remove('activo');
+          btnRevelar.disabled = true;
+          numIndice=0;
         }else{
-          document.getElementById('btnRevelarRespuesta').classList.add('disabled');
+          btnRevelar.disabled = false;
         }
-          
-        document.getElementById('respuesta1').classList.remove('activo');
-        document.getElementById('respuesta2').classList.remove('activo');
-        document.getElementById('respuesta3').classList.remove('activo');
-        document.getElementById('respuesta4').classList.remove('activo');
-        switch((num+1)){
+        switch((numIndice)){
           case 1:
-          document.getElementById('respuesta1').classList.add('activo');
+          respuesta1.classList.add('activo');
+          respuesta2.classList.remove('activo');
+          respuesta3.classList.remove('activo');
+          respuesta4.classList.remove('activo');
           break;
           case 2:
-          document.getElementById('respuesta2').classList.add('activo');
+          respuesta2.classList.add('activo');
+          respuesta1.classList.remove('activo');
+          respuesta3.classList.remove('activo');
+          respuesta4.classList.remove('activo');
+
           break;
           case 3:
-          document.getElementById('respuesta3').classList.add('activo');
+          respuesta3.classList.add('activo');
+          respuesta1.classList.remove('activo');
+          respuesta2.classList.remove('activo');
+          respuesta4.classList.remove('activo');
+
           break;
           case 4:
-          document.getElementById('respuesta4').classList.add('activo');
+          respuesta4.classList.add('activo');
+          respuesta1.classList.remove('activo');
+          respuesta2.classList.remove('activo');
+          respuesta3.classList.remove('activo');
           break;
-
         }
- 
       }
-
-     
-
-
     },
    
     mounted: function () {
-      this.cargarPregunta(0)
+      this.cargarPregunta(2);
     }  
   
   });
--- question.js
+++ question.js
@@ -25,7 +25,7 @@ nivel1 = [
       }
     ],
     "pista": "Puedes utilizar tu Biblia para buscar la respuesta en la cita Genesis Capitulo 6 entre los versiculos 1 al 20",
-    "utilizada":false
+    "utilizada": false
   },
   {
     "id": 2,
@@ -53,189 +53,233 @@ nivel1 = [
       }
     ],
     "pista": "Puedes utilizar la Biblia para buscar esta respuesta entre Génesis Capítulo 3 y Capítulo 4.",
-    "utilizada":false
+    "utilizada": false
+  },
+  {
+    "id": 3,
+    "pregunta": "¿Por cuanto tiempo estuvo Daniel en el foso de los leones?",
+    "respuestas": [
+      {
+        "id": 1,
+        "respuesta": "Una noche",
+        "correcta": true
+      },
+      {
+        "id": 2,
+        "respuesta": "una hora",
+        "correcta": false
+      },
+      {
+        "id": 3,
+        "respuesta": "Tres días",
+        "correcta": false
+      },
+      {
+        "id": 4,
+        "respuesta": "Cuatro días",
+        "correcta": false
+      }
+    ],
+    "pista": "Puedes utilizar la Biblia para buscar la respuesta Daniel Capítulo 6.",
+    "utilizada": false
+  },
+  {
+    "id": 4,
+    "pregunta": "¿Cuántas piedras tomó David para enfrentar a Goliat?",
+    "respuestas": [
+      {
+        "id": 1,
+        "respuesta": "Cinco (5) Piedras",
+        "correcta": true
+      },
+      {
+        "id": 2,
+        "respuesta": "Tres (3) Piedras",
+        "correcta": false
+      },
+      {
+        "id": 3,
+        "respuesta": "Cuatro (4) Piedras",
+        "correcta": false
+      },
+      {
+        "id": 4,
+        "respuesta": "Una (1) Piedra",
+        "correcta": false
+      }
+    ],
+    "pista": "Puedes utilizar la Biblia para buscar esta respuesta en 1 Samuel Capítulo 17.",
+    "utilizada": false
+  },
+  {
+    "id": 5,
+    "pregunta": "¿Quién sobrevivió a un horno ardiente?",
+    "respuestas": [
+      {
+        "id": 1,
+        "respuesta": "Pedro",
+        "correcta": false
+      },
+      {
+        "id": 2,
+        "respuesta": "Sadrac",
+        "correcta": true
+      },
+      {
+        "id": 3,
+        "respuesta": "Daniel",
+        "correcta": false
+      },
+      {
+        "id": 4,
+        "respuesta": "David",
+        "correcta": false
+      }
+    ],
+    "pista": "Puedes utilizar tu Biblia para buscar esta respuesta en el Libro de Daniel Capitulo 3 versiculos 1 al 30.",
+    "utilizada": false
+  },
+  {
+    "id": 6,
+    "pregunta": "¿Quién fue capturado y llevado a Babilonia?",
+    "respuestas": [
+      {
+        "id": 1,
+        "respuesta": "Jose",
+        "correcta": false
+      },
+      {
+        "id": 2,
+        "respuesta": "Isaias",
+        "correcta": false
+      },
+      {
+        "id": 3,
+        "respuesta": "Daniel",
+        "correcta": true
+      },
+      {
+        "id": 4,
+        "respuesta": "Jonas",
+        "correcta": false
+      }
+    ],
+    "pista": "Puedes utilizar tu Biblia para buscar esta respuesta en el Libro de Daniel Capitulo 1 versiculos 1 al 10.",
+    "utilizada": false
+  },
+  {
+    "id": 7,
+    "pregunta": "Esta mujer se rio de la promesa de Dios que tendría un hijo",
+    "respuestas": [
+      {
+        "id": 1,
+        "respuesta": "Ana",
+        "correcta": false
+      },
+      {
+        "id": 2,
+        "respuesta": "Rebeca",
+        "correcta": false
+      },
+      {
+        "id": 3,
+        "respuesta": "Raquel",
+        "correcta": false
+      },
+      {
+        "id": 4,
+        "respuesta": "Sara",
+        "correcta": true
+      }
+    ],
+    "pista": "Puedes utilizar la Biblia para buscar esta respuesta en Génesis Capítulo 18.",
+    "utilizada": false
+  },
+  {
+    "id": 8,
+    "pregunta": "¿Qué planta maldijo Jesús por no producir fruto?",
+    "respuestas": [
+      {
+        "id": 1,
+        "respuesta": "Olivo",
+        "correcta": false
+      },
+      {
+        "id": 2,
+        "respuesta": "Vina",
+        "correcta": false
+      },
+      {
+        "id": 3,
+        "respuesta": "Higuera",
+        "correcta": true
+      },
+      {
+        "id": 4,
+        "respuesta": "Olmo",
+        "correcta": false
+      }
+    ],
+    "pista": "Puedes utilizar tu Biblica para buscar la respuesta en la cita 2 Reyes Capitulo 2 entre los versiculos 1 al 20.",
+    "utilizada": false
+  },
+  {
+    "id": 9,
+    "pregunta": "El tercer mandamiento dice",
+    "respuestas": [
+      {
+        "id": 1,
+        "respuesta": "No tendrás dioses",
+        "correcta": false
+      },
+      {
+        "id": 2,
+        "respuesta": "No tomar nombre de Jehova en vano",
+        "correcta": true
+      },
+      {
+        "id": 3,
+        "respuesta": "No te haras imagen",
+        "correcta": false
+      },
+      {
+        "id": 4,
+        "respuesta": "No robarás",
+        "correcta": false
+      }
+    ],
+    "pista": "Puedes utilizar la Biblia para buscar esta respuesta en Exodo Capítulo 20.",
+    "utilizada": false
+  },
+  {
+    "id": 10,
+    "pregunta": "Este hombre podía convertir su vara en serpiente.",
+    "respuestas": [
+      {
+        "id": 1,
+        "respuesta": "Elias",
+        "correcta": false
+      },
+      {
+        "id": 2,
+        "respuesta": "Aaron",
+        "correcta": false
+      },
+      {
+        "id": 3,
+        "respuesta": "Moises",
+        "correcta": true
+      },
+      {
+        "id": 4,
+        "respuesta": "Daniel",
+        "correcta": false
+      }
+    ],
+    "pista": "Puedes utilizar la Biblia para buscar esta respuesta en Exodo Capítulo 20.",
+    "utilizada": false
   }
 ]
-    
-    
-    /*
-    ,
-    {
-      "id": 3,
-      "question": "¿Por cuanto tiempo estuvo Daniel en el foso de los leones?",
-      "answers": [
-        "[Una noche]",
-        "una hora",
-        "tres dias",
-        "cuatro dias"
-      ],
-      "hint": "Puedes utilizar la Biblia para buscar la respuesta Daniel Capítulo 6",
-      
-    },
-    {
-      "id": 4,
-      "question": "Cuántas piedras tomó David para enfrentar a Goliat",
-      "answers": [
-        "[5]",
-        "3",
-        "4",
-        "1"
-      ],
-      "hint": "Puedes utilizar la Biblia para buscar esta respuesta en 1 Samuel Capítulo 17"
-    },
-    {
-      "id": 5,
-      "question": "Quién sobrevivió a un horno ardiente",
-      "answers": [
-        "[Sadrac]",
-        "Pedro",
-        "Daniel",
-        "David"
-      ],
-      "hint": "Puedes utilizar tu Biblia para buscar esta respuesta en el Libro de Daniel Capitulo 3 versiculos 1 al 30"
-    },
-    {
-      "id": 6,
-      "question": "¿Quién fue capturado y llevado a Babilonia?",
-      "answers": [
-        "Jose",
-        "Isaias",
-        "[Daniel]",
-        "Joas"
-      ],
-      "hint": "Puedes utilizar tu Biblia para buscar esta respuesta en el Libro de Daniel Capitulo 1 versiculos 1 al 10"
-    },
-    {
-      "id": 7,
-      "question": "Esta mujer se rio de la promesa de Dios que tendría un hijo",
-      "answers": [
-        "[Sara]",
-        "Raquel",
-        "Rebeca",
-        "Ana"
-      ],
-      "hint": "Puedes utilizar la Biblia para buscar esta respuesta en Génesis Capítulo 18."
-    },
-    {
-      "id": 8,
-      "question": "Qué planta maldijo Jesús por no producir fruto",
-      "answers": [
-        "Olivo",
-        "Vina",
-        "[Higuera]",
-        "Olmo"
-      ],
-      "hint": "Puedes utilizar tu Biblica para buscar la respuesta en la cita 2 Reyes Capitulo 2 entre los versiculos 1 al 20"
-    },
-    {
-      "id": 9,
-      "question": "El tercer mandamiento dice",
-      "answers": [
-        "No tendrás dioses",
-        "[No tomar nombre de Jehova en vano]",
-        "No te haras imagen",
-        "No robarás"
-      ],
-      "hint": "Puedes utilizar la Biblia para buscar esta respuesta en Exodo Capítulo 20"
-    },
-    {
-      "id": 10,
-      "question": "Este hombre podía convertir su vara en serpiente",
-      "answers": [
-        "Elias",
-        "Aaron",
-        "[Moises]",
-        "Daniel"
-      ],
-      "hint": "Puedes utilizar tu Biblica para buscar la respuesta en la cita Exodo Capitulo 7 entre los versiculos 1 al 15"
-    }
-  ];
-  nivel2 = [{ 
-    "id" : 1, "question" : "Que edad tenía Abraham cuando Sara dio a luz ", 
-    "answers" : [ "50", "[100]", "120", "95"], 
-    "hint" : "Puedes utilizar la Biblia para buscar esta respuesta en Capítulo 21"
-  },{
-    "id" : 2, "question" : "¿De que sano Jesús a la suegra de Pedro?",
-    "answers" : [ "[Fiebre]", "Seguera", "Dolor de estomago", "Lepra"], 
-    "hint" : "Puedes utilizar la Biblia para buscar esta respuesta en Mateo Capítulo 8."
-  },{ 
-    "id" : 3, "question" : "¿De dónde era Goliat?", 
-    "answers" : [ "[de Gat]", "de Ur", "de Golia", "de Egipto"], 
-    "hint" : "Puedes utilizar tu Biblia para buscar esta respuesta en el Libro de 1 Samuel 17"
-  },{
-    "id" : 4, "question" : "¿Las nueras de Noemí eran?",
-    "answers" : [ "[Orfa y Rut]", "Rut y Lidia", "Ana y Rut", "Rut y Raquel"], 
-    "hint" : "Puedes utilizar la Biblia para buscar esta respuesta en Rut Capítulo 4."
-  },{ 
-    "id" : 5, "question" : "Porqué la mujer de Lot se convirtió en estatua de sal", 
-    "answers" : [ "Mintio a un Angel", "[Miro hacia atras]", "Robo idolos de oro", "Mintio a su esposo"], 
-    "hint" : "Puedes utilizar la Biblia para buscar esta respuesta en Génesis Capítulo 19"
-  },{ 
-    "id" : 6, "question" : "Dios cambió el nombre de Jacob por", 
-    "answers" : [ "Judá", "Abel", "[Israel]", "Joas"], 
-    "hint" : "Puedes utilizar tu Biblica para buscar la respuesta en la cita Genesis Genesisi Capitulo 32 entre los versiculos 1 al 30",
-  },{ 
-    "id" : 7, "question" : "Que pidió Eliseo a Elías", 
-    "answers" : [ "Que lo llevara con el ", "El manto de Elias", "[ porcion de su espiritu]", "Que lo esperara"], 
-    "hint" : "Este es una pista muy simple"
-  },{
-    "id" : 6, "question" : "Esta mujer y su esposo fabricaban tiendas",
-    "answers" : [ "[Marta]", "Jezabel", "Dalila", "Priscilla"], 
-    "hint" : "Puedes utilizar la Biblia para buscar esta respuesta en Hechos Capítulo 18."
-  },{
-    "id" : 4, "question" : "Esta mujer cuidó a su hermano en las corrientes del Nilo",
-    "answers" : [ "[Lea]", "María", "Rebeca", "Raquel"], 
-    "hint" : "Puedes utilizar la Biblia para buscar esta respuesta en Exodo Capítulo 2"
-  },{
-    "id" : 1, "question" : "¿Como se llamaba la madre de Ismael el primer hijo de Abraham?",
-    "answers" : [ "[Agar]", "Ana", "Sara", "Rut"], 
-    "hint" : "Puedes utilizar la Biblia para buscar esta respuesta en Génesis Capítulo 16"
-  }];
 
-  nivel3 = [{
-    "id" : 4, "question" : "Esta mujer murió cuando fue tirada de una ventana",
-    "answers" : [ "La bruja de Endor", "Dalila", "[Jezabel]", "Atalía"], "hint" : "Puedes utilizar la Biblia para buscar esta respuesta en 2 Ryes Capítulo 2."
-  },{
-    "id" : 1, "question" : "¿Cómo se llamo la esposa de José hijo de Jacob?",
-    "answers" : [ "[Asenat]", "Safnat-Panea", "Potifera", "Aranat"], "hint" : "Puedes utilizar la Biblia para buscar esta respuesta en Génesis Capítulo 41."
-  },{
-    "id" : 3, "question" : "Por esta mujer murieron todos los hombres de Siquem",
-    "answers" : [ "[Dina]", "Tamar", "Raquel", "Dalila"], "hint" : "Puedes utilizar la Biblia para buscar esta respuesta en Génesis Capítulo 34."
-  },{
-    "id" : 13, "question" : "¿Cuantos años tenía Sara cuando murió?",
-    "answers" : [ "[127]", "115", "130", "145"], "hint" : "Puedes utilizar la Biblia para buscar esta respuesta en Génesis 23."
-  }];
-
-
-
-/*
-{
-  "id": 1,
-  "pregunta": "Noe tuvo tres hijos",
-  "respuestas": [
-    {
-      "id": 1,
-      "respuesta": "Set Can Jafet",
-      "correcta": true
-    },
-    {
-      "id": 2,
-      "respuesta": "Levi Zabulon Jafet",
-      "correcta": false
-    },
-    {
-      "id": 3,
-      "respuesta": "Cain Abel Set",
-      "correcta": false
-    },
-    {
-      "id": 4,
-      "respuesta": "Set Can Abel",
-      "correcta": false
-    }
-  ],
-  "pista": "Puedes utilizar tu Biblia para buscar la respuesta en la cita Genesis Capitulo 6 entre los versiculos 1 al 20"
-}
-
-http://jsonviewer.stack.hu/
-*/ 
+// http://jsonviewer.stack.hu/
+//http://jsoneditoronline.org/
